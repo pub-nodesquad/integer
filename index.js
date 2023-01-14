fdad73b98929a8e9d6f26c8025e7d2e023df5c11
Romi Kusuma Bakti <romikusumab@gmail.com>
2023-01-14 21:03:28 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { readFile, writeFile } from "node:fs/promises";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/api/baca", async (_req, res) => {
+  const text = await readFile("./data.txt", "utf8");
+  res.send(text);
+});
+
+app.post("/api/tulis", async (req, res) => {
+  await writeFile("./data.txt", req.body.text, "utf8");
+  res.send(`Berhasil menulis "${req.body.text}" ke file data.txt.`);
+});
+
+app.listen(3000, () => console.log("Server berjalan."));
