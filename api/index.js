d3daafa48f2ec6e84967d1ecacf383e1e8864b7a
Tri Wulandari <triwulandari3456@gmail.com>
2023-01-21 23:00:32 +0700

    finish

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ import { readFile, writeFile } from "node:fs/promises";
 
 const app = express();
 
+const data = ["Tri Wulandari", "Banyumas"];
+
 app.use(express.static("public"));
 app.use(express.json());
 
@@ -18,6 +20,14 @@ app.get("/api/baca", async (_req, res) => {
 app.post("/api/tulis", async (req, res) => {
   await writeFile("./data.txt", req.body.text, "utf8");
   res.send(`Berhasil menulis "${req.body.text}" ke file data.txt.`);
+});
+
+app.post("/api/wulan", (req, _res) => {
+  data.push(req.body.text);
+});
+
+app.get("/api/wulan", (req, res) => {
+  res.send(data);
 });
 
 app.listen(3000, () => console.log("Server berjalan."));
