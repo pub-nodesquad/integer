45836127e302b0fabad0126cf4a7fc3a762883cf
DwiFauzul07 <fauzultanjung05@gmail.com>
2023-01-21 21:02:29 +0700

    Selesai

--- api/index.js
+++ api/index.js
@@ -20,4 +20,14 @@ app.post("/api/tulis", async (req, res) => {
   res.send(`Berhasil menulis "${req.body.text}" ke file data.txt.`);
 });
 
+const bebas = ["ayah", "kudri"];
+app.get ("/api/fauzul", (req, res) => {
+  res.send(bebas);
+});
+
+const post = ["saya", "fauzul"];
+app.post("/api/fauzul", (req, res) => {
+  post.push(req.body.tampan);
+})
+
 app.listen(3000, () => console.log("Server berjalan."));
