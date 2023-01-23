edfaaa5f3207a6f718de6be2fa3a20c8bf55a1b5
Azhari Fahreza Lubis <azharilubis3110@gmail.com>
2023-01-24 00:32:25 +0700

    lubis-contributions

--- api/index.js
+++ api/index.js
@@ -19,5 +19,14 @@ app.post("/api/tulis", async (req, res) => {
   await writeFile("./data.txt", req.body.text, "utf8");
   res.send(`Berhasil menulis "${req.body.text}" ke file data.txt.`);
 });
+const data = ["first","second"]
+app.post("/api/azhari", async(req,res)=>{
+  data.post(req.body.text);
+  res.send("Succeed")
+})  
+
+app.get("/api/azhari",async (_req,res)=>{
+  res.send(data);
+})
 
 app.listen(3000, () => console.log("Server berjalan."));
