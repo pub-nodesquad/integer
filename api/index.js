c8bc8c966217da83a5df59d21b2267ffa78fc421
aliyarohayasiregar <@aliyarohayasiregar123@gmail.com>
2023-01-25 13:30:33 +0700

    menabah tugas aliya

--- api/index.js
+++ api/index.js
@@ -15,9 +15,23 @@ app.get("/api/baca", async (_req, res) => {
   res.send(text);
 });
 
+
 app.post("/api/tulis", async (req, res) => {
   await writeFile("./data.txt", req.body.text, "utf8");
   res.send(`Berhasil menulis "${req.body.text}" ke file data.txt.`);
 });
 
+const array=["aliya","rohaya","siregar"];
+
+app.get("/api/aliya",async(_req,res)=>{
+  res.send(array);
+ });
+
+ app.post("/api/aliya",async(req,res)=>{
+  array.push(req.body);
+  array.forEach(e=> {
+    console.log(e);
+  });
+ });
+  
 app.listen(3000, () => console.log("Server berjalan."));
