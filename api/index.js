ee1c257ba951e2b66efcebaf9686e7eb1bef35e1
pasaribuputri <pasaribuputri02@gmail.com>
2023-01-25 10:49:54 +0700

    tugas putri mulyani

--- api/index.js
+++ api/index.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(express.static("public"));
 app.use(express.json());
 
+const bio = ["Putri Mulyani Pasaribu"]
+
 app.get("/api/hello", async (_req, res) => {
   res.send("Selamat datang di Integer!");
 });
@@ -19,5 +21,13 @@ app.post("/api/tulis", async (req, res) => {
   await writeFile("./data.txt", req.body.text, "utf8");
   res.send(`Berhasil menulis "${req.body.text}" ke file data.txt.`);
 });
+
+app.post("/api/putmul",(req,res)=>{
+  bio.push(req.body.text)
+})
+
+app.get("/api/putmul",(req,res)=>{
+  res.send(bio)
+})
 
 app.listen(3000, () => console.log("Server berjalan."));
