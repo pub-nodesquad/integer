8954486a8ceca9124402fe047d8051f969cdf94f
anandaprtmaa <anandapratama334@gmail.com>
2023-01-27 22:31:30 +0700

    Kuis

--- api/index.js
+++ api/index.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(express.static("public"));
 app.use(express.json());
 
+const uzumy = [];
+
 app.get("/api/hello", async (_req, res) => {
   res.send("Selamat datang di Integer!");
 });
@@ -18,6 +20,15 @@ app.get("/api/baca", async (_req, res) => {
 app.post("/api/tulis", async (req, res) => {
   await writeFile("./data.txt", req.body.text, "utf8");
   res.send(`Berhasil menulis "${req.body.text}" ke file data.txt.`);
+});
+
+app.post("/api/nan/post", (req, res) => {
+  uzumy.push(req.body.text);
+  res.send(uzumy);
+});
+
+app.get("/api/nan/get", (_req, res) => {
+  res.send(uzumy);
 });
 
 app.listen(3000, () => console.log("Server berjalan."));
